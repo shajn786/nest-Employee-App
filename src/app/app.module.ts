75710030ef5708b318e8925d5abe5af0affab872
shajn786 <shajn786@gmail.com>
2022-12-11 18:07:34 +0530

    employee login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { EmployeeLoginComponent } from './employee-login/employee-login.component';
+
+const myRoutes: Routes=[
+  {
+path:"",component:AdminLoginComponent
+},
+{
+  path:"emplogin",component:EmployeeLoginComponent
+}
+]
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminLoginComponent,
+    EmployeeLoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoutes),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/employee-login/employee-login.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-login',
+  templateUrl: './employee-login.component.html',
+  styleUrls: ['./employee-login.component.css']
+})
+export class EmployeeLoginComponent {
+
+username=""
+password=""
+
+readValues=()=>
+
+{
+  let data:any={"username":this.username,"password":this.password}
+  console.log(data)
+}
+
+}
